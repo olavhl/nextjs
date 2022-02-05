91e075b7c04d16c2f703a76f2e2988ddf9fcbf65
Olav Hartwedt Larsen <ohalar@gmail.com>
2022-02-05 18:17:47 +0100

    Showing each cat when clicking on their card

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from './Card.module.css'
+import {useRouter} from "next/router";
 
 export const Card = ({name, phone, email, image, id}) => {
+    const router = useRouter()
+
     return (
         <div
             className={styles.card}
+            onClick={() => router.push(`/cats/${id}`)}
         >
             <div className={styles["card-header"]}>
                 <img src={image.url} alt={image.alt} className={styles["card-img"]}/>
---
+++ pages/cats/[id].js
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from "react";
+import {useRouter} from "next/router";
+import {Nav} from "../../components/Nav/Nav";
+
+export default function Cat() {
+    const [cat, setCat] = useState()
+
+    const router = useRouter();
+    const {id} = router.query;
+
+    const fetchCat = async () => {
+        const res = await fetch(`/api/cats/${id}`)
+        const data = await res.json()
+        setCat(data)
+    }
+
+    useEffect(() => {
+        fetchCat().then(() => console.log("Cat Fetched"))
+    }, [])
+
+    return <div>
+        <Nav />
+        {cat && (
+            <div className={"container mt-5"} style={{display: "flex"}}>
+                <img src={cat.image.url} alt={cat.image.alt}/>
+                <div className={"mx-5"}>
+                    <h1>{cat.name}</h1>
+                    <p>{cat.description}</p>
+                </div>
+            </div>
+        )}
+    </div>
+}
