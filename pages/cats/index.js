0cacf699cef0fb96941dc85aedd1ec5a8d4eeaff
Olav Hartwedt Larsen <ohalar@gmail.com>
2022-02-05 17:45:27 +0100

    Created a card-component for displaying cats. Ready for API-call

--- pages/cats/index.js
+++ pages/cats/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import {Nav} from "../../components/Nav/Nav";
+import {Card} from "../../components/Card/Card";
 
 export default function index() {
     return <>
         <Nav />
-        <h1>Catss</h1></>
+        <h1>Catss</h1>
+        <Card email={"ola@nordmann.no"} id={1} image={""} name={"Ola"} phone={"91813339"} />
+    </>
+
 }
