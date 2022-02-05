372f1dd057e35322d08bb00efaa61b361344b733
Olav Hartwedt Larsen <ohalar@gmail.com>
2022-02-05 18:05:08 +0100

    Fetching cats from backend to fronted

--- pages/cats/index.js
+++ pages/cats/index.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Nav} from "../../components/Nav/Nav";
 import {Card} from "../../components/Card/Card";
 
-export default function index() {
+export default function Index() {
+    const [cats, setCats] = useState([])
+
+    const fetchCats = async () => {
+        const res = await fetch("/api/cats")
+        const data = await res.json()
+        console.log(data)
+        setCats(data)
+    }
+
+    useEffect(() => {
+        fetchCats().then(() => console.log("Cats fetched"))
+    }, [])
+
     return <>
         <Nav />
-        <h1>Catss</h1>
-        <Card email={"ola@nordmann.no"} id={1} image={""} name={"Ola"} phone={"91813339"} />
+        <div className={'container mt-5'}>
+            <div style={{display: 'flex', flexWrap: 'wrap'}}>
+
+            {cats.map(cat => <Card key={cat.id} email={cat.email} id={cat.id} phone={cat.phone} name={cat.name} image={cat.image} /> )}
+            </div>
+        </div>
+
     </>
 
 }
