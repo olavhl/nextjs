9098261f851cea36e6fe9b925f2ec7117aaaebb9
Olav Hartwedt Larsen <ohalar@gmail.com>
2022-02-05 18:09:34 +0100

    Implemented some navigation for homescreen and nav

--- components/Jumbotron/Jumbotron.js
+++ components/Jumbotron/Jumbotron.js
@@ -1,12 +1,16 @@
 import React from "react";
+import {useRouter} from "next/router";
 
 export const Jumbotron = () => {
+
+    const router = useRouter();
+
     return (
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
                 <h1 className="display-4">Cats World</h1>
                 <p className="lead">Find yor best friend today. Adopt a wonder cat.</p>
-                <button type="button" className="btn btn-primary">Start Looking</button>
+                <button onClick={() => router.push("/cats")} type="button" className="btn btn-primary">Start Looking</button>
 
             </div>
         </div>
--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Link from 'next/link';
 
 export const Nav = () => {
     return (
         <nav className="navbar navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <Link href={"/"} className="navbar-brand">Navbar</Link>
             </div>
         </nav>
     )
