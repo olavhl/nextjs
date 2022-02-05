39971d807dcc0da18ced5436bbbbda019b3850ce
Olav Hartwedt Larsen <ohalar@gmail.com>
2022-02-05 17:37:15 +0100

    Create new page for Cats

--- components/Jumbotron/Jumbotron.js
+++ components/Jumbotron/Jumbotron.js
@@ -7,6 +7,7 @@ export const Jumbotron = () => {
                 <h1 className="display-4">Cats World</h1>
                 <p className="lead">Find yor best friend today. Adopt a wonder cat.</p>
                 <button type="button" className="btn btn-primary">Start Looking</button>
+
             </div>
         </div>
     )
---
+++ pages/cats/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {Nav} from "../../components/Nav/Nav";
+
+export default function index() {
+    return <>
+        <Nav />
+        <h1>Catss</h1></>
+}
