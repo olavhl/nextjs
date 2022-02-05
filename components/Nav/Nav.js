ec40d6e1391d5a3db6780ca921cb71581ffd5376
Olav Hartwedt Larsen <ohalar@gmail.com>
2022-02-05 17:17:11 +0100

    Installed bootstrap and made my first component

---
+++ components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Nav = () => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+            </div>
+        </nav>
+    )
+}
